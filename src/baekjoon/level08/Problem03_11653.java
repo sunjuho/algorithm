3848f3cfbb5078b5ec0e35df203d4a5a9f100ceb
sunjuho <sun_juho@naver.com>
2022-11-10 17:11:31 +0900

    level08_기본 수학 2_소인수분해

--- src/baekjoon/level08/Problem03_11653.java
+++ src/baekjoon/level08/Problem03_11653.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 
 /*
 https://www.acmicpc.net/problem/11653
+
+정수 N이 주어졌을 때, 소인수분해하는 프로그램을 작성하시오.
  */
 public class Problem03_11653 {
     public static void main(String[] args) throws NumberFormatException, IOException {
