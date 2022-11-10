4049a1cdf80633ba61d7a6a53e4491ec06edf852
sunjuho <sun_juho@naver.com>
2022-11-10 17:10:04 +0900

    level08_기본 수학 2_소수 찾기

--- src/baekjoon/level08/Problem02_2581.java
+++ src/baekjoon/level08/Problem02_2581.java
@@ -7,6 +7,9 @@ import java.util.Arrays;
 
 /*
 https://www.acmicpc.net/problem/2581
+
+자연수 M과 N이 주어질 때 M이상 N이하의 자연수 중 소수인 것을 모두 골라 이들 소수의 합과 최솟값을 찾는 프로그램을 작성하시오.
+예를 들어 M=60, N=100인 경우 60이상 100이하의 자연수 중 소수는 61, 67, 71, 73, 79, 83, 89, 97 총 8개가 있으므로, 이들 소수의 합은 620이고, 최솟값은 61이 된다.
  */
 public class Problem02_2581 {
     public static void main(String[] args) throws NumberFormatException, IOException {
@@ -25,7 +28,8 @@ public class Problem02_2581 {
             // i가 소수이면,
             if (isPrime[i]) {
                 // i의 배수는 소수가 아님.
-                for (int j = i * i; j <= isPrime.length; j += i) { // i가 5인 경우. 2 * 5, 3 * 5, 4 * 5는 이미 2와 3에서 false로 처리되었음.
+                // i가 5인 경우. 2 * 5, 3 * 5, 4 * 5는 이미 2와 3에서 false로 처리되었음.
+                for (int j = i * i; j <= isPrime.length; j += i) {
                     isPrime[j] = false;
                 }
             }
