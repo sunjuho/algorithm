607f7093eb793c76f72d78cc98d4e4657e16f0ee
sunjuho <sun_juho@naver.com>
2022-12-08 15:19:28 +0900

    level10_정렬_수 정렬하기

---
+++ src/baekjoon/level10/Problem01_2750.java
@@ -0,0 +1,34 @@
+package baekjoon.level10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+https://www.acmicpc.net/problem/2750
+
+N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+ */
+public class Problem01_2750 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int caseN = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[caseN];
+
+        for (int i = 0; i < caseN; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (Integer num : arr) {
+            sb.append(num).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
